7156306706d0ec3109b0e4808e451523828b5201
김정욱 <@MuSI>
2015-11-05 11:28:43 +0900

    setPriority

--- JavaPractice/src/org/dimigo/thread/Race.java
+++ JavaPractice/src/org/dimigo/thread/Race.java
@@ -4,7 +4,9 @@ public class Race {
 
 	public static void main(String[] args) {
 		Runner r1 = new Runner("정성민");
+		r1.setPriority(Thread.MAX_PRIORITY);
 		Runner r2 = new Runner("정성운");
+		r2.setPriority(Thread.MIN_PRIORITY);
 		
 		r1.start();
 		r2.start();
